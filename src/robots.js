c58ef96e62588b90c2d6a47bb07a9ceb71df0534
bedulrzl <aprizalbedul@gmail.com>
2018-04-25 20:58:07 +0700

    adding starting project

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+	{
+		id:1,
+		name: 'leane',
+		username: 'leane grahan',
+		email:'abc@gmail.com'
+	},
+	{
+		id:2,
+		name: 'leane2',
+		username: 'leane2 grahan',
+		email:'abcd@gmail.com'
+	},
+	{
+		id:3,
+		name: 'leane3',
+		username: 'leane3 grahan',
+		email:'abcde@gmail.com'
+	},
+	{
+		id:4,
+		name: 'leane4',
+		username: 'leane4 grahan',
+		email:'abcdef@gmail.com'
+	},
+	{
+		id:5,
+		name: 'leane5',
+		username: 'leane5 grahan',
+		email:'abcdefg@gmail.com'
+	}
+]
